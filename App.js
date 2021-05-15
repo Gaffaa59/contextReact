5e59dd6a81c0c0facc6507df3c7c3e6647f9500e
Gaffaa59 <55506308+Gaffaa59@users.noreply.github.com>
2021-05-15 22:54:47 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from "react";
+import MyForm from './components/FormMy';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyForm />
+      <div id="Content">
+      </div>
+    </div>
+  );
+}
+
+export default App;
